ebcc415b136abc5cc8066c6d555357d50c690657
Keith R. Davis <keithrdavis@users.noreply.github.com>
2020-05-04 09:50:52 -0700

    Added main class

--- controller.py
+++ controller.py
@@ -1,4 +1,12 @@
 # coding: utf-8
 
 from scene import *
+import sound
+import random
 
+class Controller (Scene):
+	def setup(self):
+		self.background_color = '#004f82'
+
+if __name__ == '__main__':
+	run(Controller(), PORTRAIT, show_fps=True)
